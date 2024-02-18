ae4c00be54b180f6976075a1dfc7fefe3a4b8b87
WebsiteBuildTesting <160359735+WebsiteBuildTesting@users.noreply.github.com>
2024-02-18 13:22:32 +0530

    updated
    toggle menu with section linking options, youtube video autoplay in hero section and previous navigation syle and logo

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var nav = document.getElementById("nav");
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "block";
